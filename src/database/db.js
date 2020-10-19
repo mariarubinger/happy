6683ee7dd9d6d92056bfb57e60a445a4c87160c2
Maria Rubinger <rubinger.maria@gmail.com>
2020-10-18 21:22:30 -0300

    Configurações - Banco de dados

--- src/database/db.js
+++ src/database/db.js
@@ -1,15 +1,11 @@
 const Database = require('sqlite-async');
 
-//Database abre e então depois que abrir passa o bando de dados. O execute vai ser um atalho pra função debaixo.
-Database.open(__dirname + '/database.sqlite').then(execute)
-
 // vou pedir pra criar uma tabela no bando de dados, caso não exista ainda
 // CREATE TABLE IF NOT EXISTS 
 
 // INTEGER = é um número inteiro
 function execute(db) {
-    
-    db.exec(`
+    return db.exec(`
         CREATE TABLE IF NOT EXISTS orphanages (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             lat TEXT,
@@ -23,5 +19,7 @@ function execute(db) {
             open_on_weekends TEXT
         );
     `)
-
 }
+
+//Database abre e então depois que abrir passa o bando de dados. O execute vai ser um atalho pra função debaixo.
+module.exports = Database.open(__dirname + '/database.sqlite').then(execute) // o resultado disso é o db
--- src/database/fakedata.js
+++ src/database/fakedata.js
@@ -4,7 +4,8 @@ module.exports = [
         lat: "-27.222633",
         lng: "-49.645587",
         name: "Lar das meninas",
-        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        about: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        whatsapp: "999646566",
         images: [
             
             "https://images.unsplash.com/photo-1597095540293-b184e2b0d044?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
@@ -21,7 +22,8 @@ module.exports = [
         lat: "-27.222633",
         lng: "-49.655587",
         name: "Lar das meninos",
-        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        about: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        whatsapp: "999646566",
         images: [
                 
             "https://images.unsplash.com/photo-1597095540293-b184e2b0d044?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
